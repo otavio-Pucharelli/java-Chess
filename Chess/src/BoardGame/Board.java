c24c62fb77d16160f2a7e0abf66fec68cb593997
Otávio Pucharelli <otaviog.m.p@gmail.com>
2024-01-09 11:29:50 -0300

    feat: adding exceptions and protections

--- Chess/src/BoardGame/Board.java
+++ Chess/src/BoardGame/Board.java
@@ -25,28 +25,41 @@ public class Board {
         return columns;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
-
-    }
-
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardExecpetion("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardExecpetion("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
-
+        if (thereIsAPiece(position)) {
+            throw new BoardExecpetion("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns; 
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardExecpetion("Position not on the board");
+        }
+        return piece(position) != null;
+    }
 
 }
